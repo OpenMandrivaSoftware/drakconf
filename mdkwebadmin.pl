94449d74d985eddc008a320a7f10b08d8b8ff383
Crispin Boylan <crisb@mandriva.org>
2016-02-07 01:41:41 +0000

    Some more 2 to 3

--- mdkwebadmin.pl
+++ mdkwebadmin.pl
@@ -23,7 +23,7 @@
 
 use lib qw(/usr/lib/libDrakX);
 use standalone;
-use ugtk2 qw(:all);
+use ugtk3 qw(:all);
 use common;
 
 my ($ent, $link);
@@ -32,24 +32,24 @@ my ($opt) = @ARGV;
 @ARGV == 1 && ($opt eq '--direct' || $opt eq '--link') or exit(1);
 
 $opt eq '--direct' and exec "$execbin";
-my $w = ugtk2->new('mdkwebadmin.pl');
+my $w = ugtk3->new('mdkwebadmin.pl');
 unless ($::isEmbedded) {
     $w->{rwindow}->set_title(N("Connect to remote Site"));
     $w->{window}->set_border_width(5);
 }  
 
 gtkadd($w->{window},
-       gtkpack_(Gtk2::VBox->new(0,5),
-		1, Gtk2::Label->new(N("Enter the webmin address server below \n")),
-		1, $ent = Gtk2::Entry->new(),
-		0, Gtk2::HSeparator->new,
+       gtkpack_(Gtk3::VBox->new(0,5),
+		1, Gtk3::Label->new(N("Enter the webmin address server below \n")),
+		1, $ent = Gtk3::Entry->new(),
+		0, Gtk3::HSeparator->new,
 		0, gtkpack(create_hbox(),
-			   gtksignal_connect(Gtk2::Button->new(N("Connect")), clicked => sub {
+			   gtksignal_connect(Gtk3::Button->new(N("Connect")), clicked => sub {
 						 $link = 'https://' . $ent->get_text() . ':10000/';
 						 exec "$execbin $link";
 					     }), 
-			   gtksignal_connect(Gtk2::Button->new(N("Abort")), clicked => \&quit))));
+			   gtksignal_connect(Gtk3::Button->new(N("Abort")), clicked => \&quit))));
 $w->{window}->show_all;
 $w->main;
 
-sub quit() { ugtk2->exit(0) }
+sub quit() { ugtk3->exit(0) }
--- menus_launcher.pl
+++ menus_launcher.pl
@@ -5,7 +5,7 @@ use lib qw(/usr/lib/libDrakX);
 use common;
 # i18n : IMPORTANT to get correct namespace (drakconf instead of libDrakX)
 BEGIN { unshift @::textdomains, 'drakconf' }
-use ugtk2 qw(:helpers :wrappers);
+use ugtk3 qw(:helpers :wrappers);
 use interactive;
 use standalone;
 
@@ -15,35 +15,35 @@ sub gtkcan_default {
 }
 
 my $bindir = "/usr/bin/";
-my $my_win = ugtk2->new('print_launcher');
+my $my_win = ugtk3->new('print_launcher');
 $my_win->{rwindow}->set_title(N("Menu Configuration Center")) unless $::isEmbedded;
 $my_win->{window}->signal_connect(delete_event => sub {
     gtkset_mousecursor_normal();
-    ugtk2->exit(0);
+    ugtk3->exit(0);
 });
-my $table = Gtk2::Table->new(3, 2, 0);
+my $table = Gtk3::Table->new(3, 2, 0);
 $table->set_border_width(5);
 $table->set_row_spacings(5);
 $table->set_col_spacings(5);
-$table->attach(Gtk2::Label->new(N("System menu")), 0, 1, 0, 1, 'fill', 'fill', 0, 0);
-my $b1 = gtksignal_connect(Gtk2::Button->new(N("Configure...")), clicked => sub { system("$bindir/menudrake --systemmenu &") });
+$table->attach(Gtk3::Label->new(N("System menu")), 0, 1, 0, 1, 'fill', 'fill', 0, 0);
+my $b1 = gtksignal_connect(Gtk3::Button->new(N("Configure...")), clicked => sub { system("$bindir/menudrake --systemmenu &") });
 $table->attach($b1, 1, 2, 0, 1, 'fill', 'fill', 0, 0);
-$table->attach(Gtk2::Label->new(N("User menu")), 0, 1, 1, 2, 'fill', 'fill', 0, 0);
+$table->attach(Gtk3::Label->new(N("User menu")), 0, 1, 1, 2, 'fill', 'fill', 0, 0);
 
-my $combo = Gtk2::OptionMenu->new;
+my $combo = Gtk3::OptionMenu->new;
 $combo->set_popdown_strings("root", sort(list_users()));
 $table->attach($combo, 2, 3, 1, 2, 'fill', 'fill', 0, 0);
-my $b2 = gtksignal_connect(Gtk2::Button->new(N("Configure...")),
+my $b2 = gtksignal_connect(Gtk3::Button->new(N("Configure...")),
                   clicked => sub { my $a = $combo->entry->get_text(); system($a eq "root" ? "$bindir/menudrake --usermenu &" : " su $a -c \"$bindir/menudrake\" &") });
 $table->attach($b2, 1, 2, 1, 2, 'fill', 'fill', 0, 0);
 
-$my_win->{window}->add(gtkpack(Gtk2::VBox->new(0, 0),
-                               Gtk2::Label->new(N("Menu Configuration Center") . N("\n\nChoose which menu you want to configure")),
-                               Gtk2::HSeparator->new,
+$my_win->{window}->add(gtkpack(Gtk3::VBox->new(0, 0),
+                               Gtk3::Label->new(N("Menu Configuration Center") . N("\n\nChoose which menu you want to configure")),
+                               Gtk3::HSeparator->new,
                                $table,
-                               Gtk2::HSeparator->new,
-                               gtkpack(gtkset_layout(Gtk2::HButtonBox->new, 'end'),
-                                       gtkcan_default(gtksignal_connect(Gtk2::Button->new_from_stock('gtk-close'), clicked => sub { ugtk2->exit(0) }), 1)
+                               Gtk3::HSeparator->new,
+                               gtkpack(gtkset_layout(Gtk3::HButtonBox->new, 'end'),
+                                       gtkcan_default(gtksignal_connect(Gtk3::Button->new_from_stock('gtk-close'), clicked => sub { ugtk3->exit(0) }), 1)
                                       ),
                               ),
                       );
@@ -51,4 +51,4 @@ $my_win->{window}->add(gtkpack(Gtk2::VBox->new(0, 0),
 
 $my_win->{window}->show_all;
 $my_win->main;
-ugtk2->exit(0);
+ugtk3->exit(0);
